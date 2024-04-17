74f4f0c6886b85d7620844ec3490078ec20a6bb1
bocyej <jacobkristopherlaurel@gmail.com>
2024-04-17 11:37:38 +0800

    fixed bottom nav

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:innolympics_sanetech/Authentication/auth_page.dart';
 import 'package:innolympics_sanetech/firebase_options.dart';
+import 'package:innolympics_sanetech/widget_tree.dart';
 
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
@@ -17,7 +17,6 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,7 +25,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey.shade900),
         useMaterial3: true,
       ),
-      home: AuthPage(),
+      home: WidgetTree(),
     );
   }
 }
